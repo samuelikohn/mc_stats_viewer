7d8f0a3dd65507f9861c5708b06b9495b72b95a1
Sam Kohn <49605907+samuelikohn@users.noreply.github.com>
2025-07-02 23:03:06 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		proxy: {
-			// All requests starting with /api will be proxied
 			'/api': {
-				target: 'https://sessionserver.mojang.com/session/minecraft/profile/', // **Replace with the actual base URL of your 3rd party API**
-				changeOrigin: true, // Needed for virtual hosted sites
-				rewrite: (path) => path.replace(/^\/api/, '') // Remove the /api prefix when forwarding the request
+				target: 'https://sessionserver.mojang.com/session/minecraft/profile/',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
 			}
 		}
   	}	
